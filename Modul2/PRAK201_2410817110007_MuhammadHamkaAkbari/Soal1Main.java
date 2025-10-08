f5e89b77cdfdf65f2f8bfb0e8f90218d82d7645c
Hamka-akbari03 <hamkaakbari41@gmail.com>
2025-10-08 16:47:55 +0800

    Add files via upload

---
+++ Modul2/PRAK201_2410817110007_MuhammadHamkaAkbari/Soal1Main.java
@@ -0,0 +1,14 @@
+package PRAK201_2410817110007_MuhammadHamkaAkbari;
+
+public class Soal1Main {
+    public static void main(String[] args) {
+
+        Fruit apple = new Fruit("Apel", 0.4, 7000.0, 40.0);
+        Fruit mango = new Fruit("Mangga", 0.2, 3500.0, 15.0);
+        Fruit avocado = new Fruit("Alpukat", 0.25, 10000.0, 12.0);
+
+        apple.displayInfo();
+        mango.displayInfo();
+        avocado.displayInfo();
+    }
+}
