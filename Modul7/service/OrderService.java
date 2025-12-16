1d83258d8a9b647846c75ae41a007c0743b641e9
Hamka-akbari03 <hamkaakbari41@gmail.com>
2025-12-16 22:48:57 +0800

    Add files via upload

---
+++ Modul7/service/OrderService.java
@@ -0,0 +1,21 @@
+package Modul7.service;
+
+import Modul7.model.Order;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class OrderService {
+    private final ObservableList<Order> orders = FXCollections.observableArrayList();
+
+    public ObservableList<Order> getOrders() {
+        return orders;
+    }
+
+    public void add(Order order) {
+        orders.add(order);
+    }
+
+    public void remove(Order order) {
+        orders.remove(order);
+    }
+}
---
+++ Modul7/ui/AlertHelper.java
@@ -0,0 +1,13 @@
+package Modul7.ui;
+
+import javafx.scene.control.Alert;
+
+public class AlertHelper {
+    public static void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
